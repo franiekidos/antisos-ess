9443001090a02cf2ddbebafcebf07cf4a6c24058
franiekidos <lukkuc7262@gmail.com>
2026-04-26 08:34:49 +0000

    modified:   build-repo.sh 	modified:   x86_64/antisos-keyring-2026.04-1-any.pkg.tar.zst 	new file:   x86_64/antisos-keyring-2026.04-1-any.pkg.tar.zst.sig 	modified:   x86_64/antisos.db 	new file:   x86_64/antisos.db.sig 	modified:   x86_64/antisos.files 	new file:   x86_64/antisos.files.sig 	modified:   x86_64/calamares-3.4.2-2-x86_64.pkg.tar.zst 	new file:   x86_64/calamares-3.4.2-2-x86_64.pkg.tar.zst.sig 	modified:   x86_64/calamares-debug-3.4.2-2-x86_64.pkg.tar.zst 	new file:   x86_64/calamares-debug-3.4.2-2-x86_64.pkg.tar.zst.sig 	modified:   x86_64/ckbcomp-1.247-1-any.pkg.tar.zst 	new file:   x86_64/ckbcomp-1.247-1-any.pkg.tar.zst.sig

--- build-repo.sh
+++ build-repo.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# Script: build-repo.sh
-# Philosophy: Compile-on-Demand (The Factory Model)
-
 set -euo pipefail
 
 REPO_NAME="antisos"
@@ -9,30 +6,21 @@ SOURCE_ROOT="../antisos-ess-build"
 TARGET_DIR="$(pwd)/x86_64"
 
 mkdir -p "$TARGET_DIR"
-
-# List of packages in the order they should be built (keyring first!)
 PACKAGES=("antisos-keyring" "calamares" "ckbcomp")
 
 for pkg in "${PACKAGES[@]}"; do
     pkg_path="$SOURCE_ROOT/$pkg"
     
-    echo "--> Sprawdzam pakiet: $pkg"
     if [ -d "$pkg_path" ]; then
         cd "$pkg_path"
+        echo "--> Kompilacja i podpisywanie: $pkg"
         
-        # Budujemy. Jeśli makepkg zawiedzie, skrypt przerwie pracę (dzięki set -e)
-        echo "--> Kompilacja: $pkg"
-        makepkg -scf --noconfirm
+        # Dodajemy --sign, aby podpisać paczkę podczas budowy
+        makepkg -scf --sign --noconfirm
         
-        # Sprawdzenie czy plik faktycznie powstał
-        BUILT_PKG=$(ls *.pkg.tar.zst 2>/dev/null | head -n 1)
-        if [ -n "$BUILT_PKG" ]; then
-            echo "--> Eksportuję $BUILT_PKG do $TARGET_DIR"
-            mv -f "$BUILT_PKG" "$TARGET_DIR/"
-        else
-            echo "!! BŁĄD: Nie znaleziono gotowej paczki dla $pkg!"
-            exit 1
-        fi
+        echo "--> Eksportuję paczki i podpisy do $TARGET_DIR"
+        # KLUCZOWA ZMIANA: Przenosimy plik .zst ORAZ .sig
+        mv -f *.pkg.tar.zst* "$TARGET_DIR/"
         
         cd - > /dev/null
     else
@@ -45,15 +33,20 @@ done
 cd "$TARGET_DIR"
 
 echo "--> Refreshing $REPO_NAME database..."
+# Czyścimy stare pliki bazy, ale ZOSTAWIAMY pliki .sig paczek
 rm -f ${REPO_NAME}.db* ${REPO_NAME}.files*
 
-# Add all packages found in the target dir to the DB
-repo-add -n -R ${REPO_NAME}.db.tar.gz *.pkg.tar.zst
+# Dodajemy flagę -s do repo-add, aby podpisać samą bazę danych
+repo-add -s -n -R ${REPO_NAME}.db.tar.gz *.pkg.tar.zst
 
-# Fix for GitHub Pages (The Symlink Fix)
+# Fix dla GitHub Pages (Symlink Fix) - Musimy przenieść też podpisy bazy!
+rm -f ${REPO_NAME}.db ${REPO_NAME}.files
+
 mv ${REPO_NAME}.db.tar.gz ${REPO_NAME}.db
+mv ${REPO_NAME}.db.tar.gz.sig ${REPO_NAME}.db.sig
 mv ${REPO_NAME}.files.tar.gz ${REPO_NAME}.files
+mv ${REPO_NAME}.files.tar.gz.sig ${REPO_NAME}.files.sig
 
 echo "#######################################"
-echo "   Build Complete & Database Updated   "
+echo "   Build Complete & Database Signed    "
 echo "#######################################"
